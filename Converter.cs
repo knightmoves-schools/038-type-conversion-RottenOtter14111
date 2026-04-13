a9f81d428680659811e65c75b25c8a385f9066f1
RottenOtter14111 <arogers@anamosa.k12.ia.us>
2026-04-13 04:03:07 +0000

    omg.

--- Converter.cs
+++ Converter.cs
@@ -1,15 +1,15 @@
 ﻿namespace knightmoves;
 
 public class Converter{
-public ConvertImplicitly()
+public ConvertImplicitly(int number)
 {
-      int number = 10;
+   
       double doublednumber = number;
 return doublednumber;
    }
-public ConvertExplicitly()
+public ConvertExplicitly(double num1)
    {
-      double num1 = 2.5;
+     
       int intnum1 = (int) num1;
 
       return intnum1;
